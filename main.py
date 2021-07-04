c077d10477068a8bf4dca92c4f19a54ae528e1e7
Okkonen <okko.oikkonen@gmail.com>
2021-07-04 21:13:15 +0300

    Main

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
+import asyncio
+import json
 import os
+import random
 import discord
-from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.ext.commands import when_mentioned_or
+from discord.ext.commands import ExtensionError
+from datetime import datetime
 
-bot = commands.Bot(command_prefix=".")
+class CogList:
 
+  cogs = [
+    "cogs.core",
+  ]
+
+
+bot = Bot(command_prefix=when_mentioned_or("b!"))
+bot.remove_command("help")
+
+bot_uptime = None
 
 @bot.event
 async def on_ready():
-  return print(f"Logged in as {bot.user}\nAvailable in servers: {bot.guilds}")
-  # TODO start status changer
+  global bot_uptime
+  print(f"Logged in as {bot.user}\nAvailable in servers: {bot.guilds[0]}")
+  bot_uptime = datetime.now() # save the timedelta when bot is activated
+  bot.loop.create_task(status_changer())
+  await load_all_cogs()
 
 
 @bot.event
-async def on_message(message):
-  print(message)
+async def on_message(message: discord.Message):
+  if not message.content:
+    return
+  if message.mentions:
+    print(message.mentions + "is based")
+    pass 
 
-# TODO add cogs
 
+
+async def load_all_cogs():
+  for cog in CogList.cogs:
+    try:
+      bot.load_extension(cog)
+      print(f"loaded ext {cog}")
+    except ExtensionError as err:
+      print(f"Failed to load ext {cog}: [{err}]")
+  return print("\nloaded cogs")
+
+
+async def status_changer():
+  with open(f'{os.path.dirname(__file__)}/config.json') as conf:
+    config = json.load(conf)
+  while True:
+    await asyncio.sleep(20)
+    activity = random.choice(config["activities"])
+    await bot.change_presence(activity=discord.Activity(name=activity["name"], type=discord.ActivityType[activity["status"]]))
 
 def main(secret):
   bot.run(secret)
